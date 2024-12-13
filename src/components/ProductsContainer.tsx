eadbdb0d3c908f797827ba80b8f5a233b86c81cb
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-12-12 21:47:10 -0300

    feat: fetching products and saving them in the products state

--- src/components/ProductsContainer.tsx
+++ src/components/ProductsContainer.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
 import ProductsList from "./ProductsList"
+import { getAllProducts } from "../services/products";
 
 const ProductsContainer = () => {
+  const [products, setProducts] = useState();
+
+  console.log(products);
+  
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const data = await getAllProducts();
+      setProducts(data);
+    }
+    fetchProducts();
+  }, []);
+
   return (
     <ProductsList/>
   )
